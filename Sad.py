c4d5fb15c8a7c7d7c3eea8cd7ffebbbfaeea0e5e
tryas <dradeev@gmail.com>
2021-11-12 13:11:13 +0300

    Initial commit

---
+++ Sad.py
@@ -0,0 +1,54 @@
+# Начните работу над проектом «Склад оргтехники». Создайте класс, описывающий склад.
+# А также класс «Оргтехника», который будет базовым для классов-наследников.
+# Эти классы — конкретные типы оргтехники (принтер, сканер, ксерокс).
+# В базовом классе определить параметры, общие для приведенных типов.
+# В классах-наследниках реализовать параметры, уникальные для каждого типа оргтехники.
+# Продолжить работу над первым заданием. Разработать методы, отвечающие за приём оргтехники
+# на склад и передачу в определенное подразделение компании. Для хранения данных о наименовании
+# и количестве единиц оргтехники, а также других данных, можно использовать любую подходящую структуру,
+# например словарь.
+
+class Store:
+
+    pass
+
+class Company:
+    pass
+
+class Department1(Company):
+    pass
+
+class Department2(Company):
+    pass
+
+class Equipment:
+    def __init__(self, brand, model, serial_number):
+        self.brand = brand
+        self.model = model
+        self.serial_number = serial_number
+
+class Printer(Equipment):
+    def __init__(self, brand, model, color, serial_number):
+        super().__init__(brand, model, serial_number)
+        self.color = color
+
+class Scaner(Equipment):
+    def __init__(self, brand, model, depth_color, serial_number):
+        super().__init__(brand, model, serial_number)
+        self.depth_color = depth_color
+
+class Xerox(Equipment):
+    def __init__(self, brand, model, document_feeder, serial_number):
+        super().__init__(brand, model, serial_number)
+        self.document_feeder = document_feeder
+
+
+
+my_printer = Printer('Epson', 'EX-800', False, 123456789)
+print(my_printer, my_printer.brand, my_printer.model, my_printer.color, my_printer.serial_number)
+
+my_scaner = Scaner('Canon', 'HG-18', 32, 987654321)
+print(my_scaner, my_scaner.brand, my_scaner.model, my_scaner.depth_color, my_scaner.serial_number)
+
+my_xerox = Xerox('Xerox', 'RT-19', True, 123498765)
+print(my_xerox, my_xerox.brand, my_xerox.model, my_xerox.document_feeder, my_xerox.serial_number)
